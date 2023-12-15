2727815e929f987653d3b07ec046efe7a79adde4
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-12-15 09:12:54 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{
+        width: '100%',
+        margin: '2rem'
+      }}>
+        <h1 style={{
+          color: 'rgba(255, 255, 255, 0.8)',
+          textAlign: 'center'
+        }}>DisplayCaseDesign
+        </h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
