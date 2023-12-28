c161d0a55014241ee50bd07d8b130601c98b81f8
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-12-28 13:15:29 -0500

    added box

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <div style={{
         width: '100%',
-        margin: '2rem'
+        margin: '2rem 0 2rem 0'
       }}>
         <h1 style={{
           color: 'rgba(255, 255, 255, 0.8)',
@@ -15,7 +15,13 @@ function App() {
         }}>DisplayCaseDesign
         </h1>
       </div>
-
+      <div style={{
+        height: '16rem',
+        width: '16rem',
+        border: 'lightblue 2px solid',
+        margin: '2rem auto',
+        backgroundColor: 'rgba(100, 100, 255, 0.5)'
+      }}></div>
     </>
   )
 }
